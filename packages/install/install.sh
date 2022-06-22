62477f56a07de29b8881bd626833530367da777d
Morteza Jamali <mortezajamali4241@gmail.com>
2022-06-22 16:03:18 +0000

    chore(actions): improve `release.yaml` github action

--- packages/install/install.sh
+++ packages/install/install.sh
@@ -4,8 +4,8 @@ ROOT_PATH=$HOME/.bpm
 
 if ! command -v bpm &>/dev/null; then
   cd $(mktemp -d)
-  curl -#LO https://github.com/morajlab/bpm/releases/download/v0.0.1-alpha/bpm-v0.0.1-alpha.zip
-  unzip bpm-v0.0.1-alpha.zip
+  curl -#LO https://github.com/morajlab/bpm/releases/download/v0.0.1-alpha.3/bpm-v0.0.1-alpha.3.zip
+  unzip bpm-v0.0.1-alpha.3.zip
   mkdir $ROOT_PATH
   mv bpm $ROOT_PATH/
 
