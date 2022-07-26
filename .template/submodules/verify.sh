c2fc51fabf56dc441be4dd70aa787bdc2a193919
Morteza Jamali <mortezajamali4241@gmail.com>
2022-07-26 03:07:06 +0000

    feat(base): create `.template` directory

---
+++ .template/submodules/verify.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo 'This is submodules template'
+
+exit 1
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+shopt -s expand_aliases
+
+ROOT_PATH=$(dirname $(realpath $(dirname $0)))
+
+alias bpm=$ROOT_PATH/packages/cli/bpm
+
+# Install bash modules
+bpm install log color
+
 # Install node modules
 npm i
 
