b78ff2b5b919018da3dd71fbdb567ca536fd8c18
Morteza Jamali <mortezajamali4241@gmail.com>
2022-08-16 06:24:33 +0000

    feat(base): create `package.sh` file

---
+++ package.sh
@@ -0,0 +1,23 @@
+NAME="morajlab/bpm"
+DESCRIPTION="Bash package manager"
+VERSION="0.0.1-alpha.6"
+BIN="packages/cli/bin"
+DEPENDENCIES=(
+  ["jq"]=("apt" "*")
+)
+LICENSE="MIT"
+KEYWORDS=(
+  "bash"
+  "script"
+  "unix"
+  "linux"
+  "package"
+  "manager"
+  "shell"
+)
+AUTHOR=(
+  ["name"]="Morteza Jamali"
+  ["email"]="mortezajamali4241@gmail.com"
+  ["website"]="https://moraj.ir"
+)
+REPOSITORY="https://github.com/morajlab/bpm"
